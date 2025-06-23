162d5a0b5d66fe88b8bd220a4a176d29db99a5ad
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-23 20:16:07 +0100

    Update patch.sh

--- 3.3.14.5/distros/plain/patch.sh
+++ 3.3.14.5/distros/plain/patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/$USER && git clone https://github.com/claudemods/arch-calamares.git
 cd /home/$USER/arch-calamares/3.3.14.5/distros/plain
-sudo pacman -U calamares-3.3.14-5-x86_64_REPACKED.pkg.tar.zst calamares-oem-kde-settings-20240616-3-any.pkg.tar calamares-tools-0.1.0-1-any.pkg.tar && sudo rm -rf /home/$USER/arch-calamares
+sudo pacman -U calamares-3.3.14-5-x86_64_REPACKED.pkg.tar.zst calamares-oem-kde-settings-20240616-3-any.pkg.tar calamares-tools-0.1.0-1-any.pkg.tar ckbcomp-1.227-2-any.pkg.tar.zst && sudo rm -rf /home/$USER/arch-calamares
