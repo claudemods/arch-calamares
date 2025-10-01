f6b1f7b1042e24f974a06b576fe20ccee4d7d159
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-01 23:18:51 +0100

    new calamares artwork

--- 3.3.14.5/distros/plain/patch.sh
+++ 3.3.14.5/distros/plain/patch.sh
@@ -3,4 +3,5 @@ cd /home/$USER && git clone https://github.com/claudemods/arch-calamares.git
 cd /home/$USER/arch-calamares/3.3.14.5/distros/plain
 sudo pacman -U calamares-3.3.14-5-x86_64_REPACKED.pkg.tar.zst calamares-oem-kde-settings-20240616-3-any.pkg.tar calamares-tools-0.1.0-1-any.pkg.tar ckbcomp-1.227-2-any.pkg.tar.zst && sudo rm -rf /home/$USER/arch-calamares
 sudo cp -r /home/$USER/arch-calamares/3.3.14.5/distros/plain/calamares /etc
+sudo cp -r /home/$USER/arch-calamares/3.3.14.5/distros/plain/claudemods /usr/share/calamares/branding
 sudo rm -rf /home/$USER/arch-calamares
