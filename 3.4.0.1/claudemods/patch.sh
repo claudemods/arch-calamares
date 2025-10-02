9ccbc65ec5a6bb9590f5c7e92324345153dfaff0
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-02 13:33:08 +0100

    Update patch.sh

--- 3.4.0.1/claudemods/patch.sh
+++ 3.4.0.1/claudemods/patch.sh
@@ -4,4 +4,5 @@ cd /home/$USER/arch-calamares/3.4.0.1/claudemods
 sudo pacman -U calamares-3.4.0-1-x86_64.pkg.tar.zst calamares-oem-kde-settings-20240616-3-any.pkg.tar calamares-tools-0.1.0-1-any.pkg.tar.zst ckbcomp-1.227-2-any.pkg.tar
 sudo cp -r calamares /etc/
 sudo cp -r claudemods /usr/share/calamares/branding/
+sudo unzip -o -q "extras.zip" -d /
 sudo rm -rf /home/$USER/arch-calamares
